e71f0d4107cf6b814dff2b13430018837699d9cc
Natan Oihman <natanoih@gmail.com>
2024-08-26 15:59:06 +0300

    fixed text url

--- api/routes/home.go
+++ api/routes/home.go
@@ -1,6 +1,8 @@
 package routes
 
 import (
+	"fmt"
+
 	"github.com/NatanOih/urlShortenerGo/database"
 	"github.com/NatanOih/urlShortenerGo/helpers"
 	"github.com/gofiber/fiber/v2"
@@ -22,7 +24,19 @@ func GetAllUrls(c *fiber.Ctx) error {
 }
 
 func TestUrl(c *fiber.Ctx) error {
+	fmt.Print("got here")
 
-	JsonForUi := "hi"
+	r := database.CreateClient(0)
+	defer r.Close()
+
+	ctx := database.Ctx
+
+	JsonForUi, err := helpers.FetchDataFromRedis(ctx, r)
+
+	if err != nil {
+		fmt.Print("got here error", err)
+		return c.Status(fiber.StatusInternalServerError).SendString("Failed to fetch data from Redis")
+	}
+
 	return c.JSON(JsonForUi)
 }
